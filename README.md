e50c21727484fb459c7a2a6044a204b0f753b386
David Castillo <castillobgr@gmail.com>
2015-08-02 11:04:06 -0500

    Added support for Arrays. Squashed camel bug.

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # dolittle.js
+Dolittle kindly translates your camelCase JSON object keys into snake_case keys, and viceversa.
+It's an angular module meant to minimize the effort in translating camel case JSON keys of JS
+clients to snake case, used in some APIs made with Ruby, for example.
+
+## Install it with npm or bower
+```sh
+$ bower install dolittle
+```
+```sh
+$ npm install dolittle
+```
+### Angular module:
+For the angular module, go [here](https://github.com/castillobg/dolittle).
+
+## Use it
+```javascript
+var dolittle = require('dolittle');
+
+var testObj = {
+  helloWorld : 'Hello World',
+  nestedObject : {
+    danger : 'Here be dragons',
+    fooBar : [1, 2, 3],
+    fooBarBaz : [
+      { test1 : 'what'},
+      { test2 : ['this', 'that']}
+    ]
+  }
+};
+
+var snakeized = dolittle.to.snake(testObj);
+// console.log(snakeized); will print:
+// {
+//   "hello_world" : "Hello World",
+//   "nested_object" : {
+//     "danger" : "Here be dragons",
+//     "foo_bar" : [1,2,3],
+//     "foo_bar_baz" : [
+//       {"test1" : "what"},
+//       {"test2" : [ "this","that"]}
+//     ]
+//   }
+// }
+
+var camelized = dolittle.to.camel(snakeized);
+// console.log(snakeized); will print the original object;
+```
---
+++ dolittle.js
@@ -0,0 +1,56 @@
+'use strict';
+
+function snake(obj){
+  return iterate(obj, function(propName){
+    return propName.replace(/([a-z0-9])([A-Z])/g, '$1_$2').toLowerCase();
+  });
+};
+
+function camel(obj){
+  return iterate(obj, function(propName){
+    return propName.replace(/^_+/, '')
+  		.replace(/_+([a-z])/g, function(_, char) {
+  			return char.toUpperCase();
+  		});
+  });
+};
+
+function iterate(obj, convert){
+  if(typeof obj !== 'object'){
+    return obj;
+  }
+  var translatedObj = {};
+  for(var prop in obj){
+    if(obj[prop] instanceof Array){
+      var translatedArray = [];
+      for(var i = 0; i < obj[prop].length; i++){
+        translatedArray.push(iterate(obj[prop][i], convert));
+      }
+      translatedObj[convert(prop)] = translatedArray;
+    }
+    else if((typeof obj[prop] !== null) && (typeof obj[prop] === 'object')){
+      translatedObj[convert(prop)] = iterate(obj[prop], convert);
+    }
+    else {
+      translatedObj[convert(prop)] = obj[prop];
+    }
+  }
+  return translatedObj;
+}
+
+var translate = {
+  'to': {
+    'snake' : snake,
+    'camel' : camel
+  }
+};
+
+var to = {
+  'snake' : snake,
+  'camel' : camel
+}
+
+module.exports = {
+  'translate': translate,
+  'to' : to
+};
